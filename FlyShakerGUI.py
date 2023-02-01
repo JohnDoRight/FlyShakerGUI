ed240a8bb631ba7594fd5d2de9be2d8698adf0ff
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2023-02-01 12:55:40 -0800

    Update FlyShakerGUI.py
    Added in Radio layout for wave type selection.

--- FlyShakerGUI.py
+++ FlyShakerGUI.py
@@ -13,3 +13,39 @@ Changelog:
 2-1-2023: Initial Creation
 """
 
+import PySimpleGUI as sg
+
+
+def main():
+    print("main")
+
+    # Setup Theme
+    sg.theme('TealMono')
+
+    # Setup Layout
+    layout = [[sg.Text('Choose a Wave Type (Sine or Pulse):')],
+              [sg.Radio("Sine", "RADIO1", default=True), sg.Radio('Pulse', "RADIO1")],
+              [sg.Button('Cancel')]
+              ]
+
+    # Create Window
+    window = sg.Window('FlyShaker GUI', layout)
+
+    # Event Loop to process "events" and get the "values" of the inputs
+
+    # While Loop
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel':
+            break
+
+        print('You entered', values[0])
+    #  Get event and values
+    #  Check for events.
+
+    # Close Window after breaking out of loop
+    window.close()
+
+
+if __name__ == "__main__":
+    main()
