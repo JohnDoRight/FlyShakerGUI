64bb682b661e5e7edf20cb5d13a9334daa50e96c
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2023-02-01 09:45:23 -0800

    Create FlyShakerGUI.py
    Initi Commit

---
+++ FlyShakerGUI.py
@@ -0,0 +1,15 @@
+"""
+Title: Fly Shaker GUI
+Author: Johnny Duong
+Creation Date: Feb 1st, 2023
+Editor: [To Future Editors, delete this note and put your name here]
+
+Description:
+GUI using Thomas Zimmerman's wave generator code for Dr. Divya Sitaraman's drosophila melanogaster sleep research.
+
+[Note: Put in Tom's CCC and IBM funding section here.]
+
+Changelog:
+2-1-2023: Initial Creation
+"""
+
