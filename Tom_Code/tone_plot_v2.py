f229f9dda813b356160fbd6a4233b756a7527f06
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2023-02-22 09:22:25 -0800

    Update tone_plot_v2.py
    Created separate functions to generate sine and square waves, tested time array separate from list comprehension (allows for longer than 1 second sound generation), and added comments.

--- Tom_Code/tone_plot_v2.py
+++ Tom_Code/tone_plot_v2.py
@@ -1,10 +1,17 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Sep  1 17:00:56 2022
 
 @author: Tom Zimmerman, IBM Research
 based on https://stackoverflow.com/questions/56592522/python-simple-audio-tone-generator
 MedoAlmasry, Egypt, June 7, 2020
+
+Direct Link to MedoAlmasry's post: https://stackoverflow.com/a/62250319
+
+@editor: Johnny Duong
+
+TODO:
+-Turn into variables/functions: sampleRate, freq, duration, fade_in, fade_out, amp
+-Modularize to work with a GUI
 
 """
 
@@ -18,7 +25,7 @@ from scipy import signal
 sampleRate = 44100
 freq = 60
 fade_in=100     # rise time of sound time in milliseconds
-duration=1000   # time in milliseconds
+duration=5000   # time in milliseconds
 fade_out=50     # fall time of sound time in milliseconds
 amp=4096        # amplitude of sound 4096 is max
 amp=16000        # MAX 32000 !
@@ -28,7 +35,7 @@ def get_sine_wave(amp, freq, sample_rate):
     # Generate sine wave with amp (how far from zero in the y direction it goes) with sampleRate x values (44100 in this case)
     #  Then normalize it so the x values will be from 0 to 1 in the x direction. Sine formula is Sin(2*pi*freq*x) TODO: Fix the formula.
     #  Generates a horizontal vector
-    arr = numpy.array([amp * numpy.sin(2.0 * numpy.pi * freq * x / sample_rate) for x in range(0, sampleRate)]).astype(numpy.int16)
+    arr = numpy.array([amp * numpy.sin(2.0 * numpy.pi * freq * x / sample_rate) for x in range(0, sample_rate)]).astype(numpy.int16)
     # Creates 2 duplicate horizontal vectors, stacks them on top of each other. TODO: Figure out why?
     arr2 = numpy.c_[arr,arr]
     return arr, arr2
@@ -38,10 +45,73 @@ def get_square_wave(amp, freq, sample_rate):
     # Generate sine wave with amp (how far from zero in the y direction it goes) with sampleRate x values (44100 in this case)
     #  Then normalize it so the x values will be from 0 to 1 in the x direction. Sine formula is Sin(2*pi*freq*x) TODO: Fix the formula.
     #  Generates a horizontal vector
-    arr = numpy.array([amp * signal.square(2.0 * numpy.pi * freq * x / sample_rate) for x in range(0, sampleRate)]).astype(numpy.int16)
+    arr = numpy.array([amp * signal.square(2.0 * numpy.pi * freq * x / sample_rate, duty=0.5) for x in range(0, sample_rate)]).astype(numpy.int16)
     # Creates 2 duplicate horizontal vectors, stacks them on top of each other. TODO: Figure out why?
     arr2 = numpy.c_[arr,arr]
     return arr, arr2
+
+
+def get_square_wave2(amp, freq, sample_rate):
+    # Generate sine wave with amp (how far from zero in the y direction it goes) with sampleRate x values (44100 in this case)
+    #  Then normalize it so the x values will be from 0 to 1 in the x direction. Sine formula is Sin(2*pi*freq*x) TODO: Fix the formula.
+    #  Generates a horizontal vector
+
+    # sampling interval
+    ts = 1.0/sample_rate
+    seconds_start = 0
+    seconds_end = 10
+    t = numpy.arange(seconds_start, seconds_end, ts)
+
+    square_wave = amp * signal.square(2.0 * numpy.pi * freq * t, duty=0.5)
+    arr = square_wave.astype(numpy.int16)
+    # Creates 2 duplicate horizontal vectors, stacks them on top of each other. TODO: Figure out why?
+    arr2 = numpy.c_[arr,arr]
+    return arr, arr2
+
+
+def main3():
+    print("main3")
+
+    pygame.mixer.init(44100,-16,1,512)
+
+    PLOT_SAMPLES=1000
+
+    # sampling rate
+    sr = 44100
+    # sampling interval
+    ts = 1.0/sr
+    t = numpy.arange(0, 1, ts)
+
+    # frequency of the signal
+    freq = 200
+    # y = numpy.sin(2*numpy.pi*freq*t)
+    y = signal.square(2*numpy.pi*freq*t, duty=0.5)
+
+    plt.figure(figsize = (8, 8))
+    plt.subplot(211)
+    plt.plot(t[0:PLOT_SAMPLES], y[0:PLOT_SAMPLES], 'b')
+    plt.ylabel('Amplitude')
+
+    # freq = 10
+    # y = numpy.sin(2*numpy.pi*freq*t)
+    amp = 16000
+    arr, arr2 = get_square_wave2(amp, freq, sr)
+    y = arr[0:PLOT_SAMPLES]
+
+    plt.subplot(212)
+    plt.plot(t[0:PLOT_SAMPLES], y, 'b')
+    plt.ylabel('Amplitude')
+
+    plt.xlabel('Time (s)')
+    plt.show()
+
+    sound = pygame.sndarray.make_sound(arr2)
+    sound.play(fade_ms=fade_in)
+    pygame.time.delay(duration)
+
+    # Both might be redundant.
+    sound.fadeout(fade_out)
+    pygame.time.wait(fade_out)
 
 
 def main2():
@@ -166,4 +236,5 @@ def main():
 
 if __name__ == "__main__":
     # main()
-    main2()
+    # main2()
+    main3()
